db6e409285a5feab86cffa52073f2a07a84151f4
John Shearer <john@ultranaut.com>
2020-12-18 18:59:40 -0500

    Make App a functional component
    Use hooks instead of lifecycle events

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchPatterns } from '../actions';
 import Details from './Details';
 import SearchBar from './SearchBar';
 import styles from './App.module.css';
 
-class App extends React.Component {
-  componentDidMount() {
-    this.props.fetchPatterns();
-  }
+const App = ({ patterns, fetchPatterns }) => {
+  useEffect(() => {
+    fetchPatterns();
+  }, [fetchPatterns]);
 
-  render() {
-    console.log(this.props.patterns);
-    return (
-      <div className={styles.App}>
-        <SearchBar />
-        <Details />
-      </div>
-    );
-  }
-}
+  console.log(patterns);
+
+  return (
+    <div className={styles.App}>
+      <SearchBar />
+      <Details />
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
