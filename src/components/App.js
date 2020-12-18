130ab376abdab50cf2f4bf2589f176898b952d63
John Shearer <john@ultranaut.com>
2020-12-17 21:44:50 -0500

    Reincorporate styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPatterns } from '../actions';
+import styles from './App.module.css';
 
 class App extends React.Component {
   componentDidMount() {
@@ -10,8 +11,15 @@ class App extends React.Component {
   render() {
     console.log(this.props.patterns);
     return (
-      <div>
-        <h2 id="title">Title</h2>
+      <div className={styles.App}>
+        <form className={styles.search}>
+          <label>Pattern keyword(s): </label>
+          <input type="text" id="searchTerm" value="begin" />
+          <input type="submit" value="search" />
+        </form>
+        <canvas className={styles.myCanvas}></canvas>
+
+        <h2 className={styles.title}>Title</h2>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
+import './index.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
